319babf5942fd92c7035a5cba5f7785c4b6f4f79
Petra Dueck <petra@node-1w7jr9qkaqait0z6ehm0tfvxs.ipv6.telus.net>
2017-06-06 15:07:26 -0700

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Petra Dueck on 2017-06-06.
+//  Copyright © 2017 Petra Dueck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
